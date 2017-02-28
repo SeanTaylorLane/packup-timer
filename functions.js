affb9d6ee9a67898bfb8533212b50281fae901f7
SeanTaylorLane <seantlane1999@gmail.com>
2017-02-28 17:07:35 -0500

    Flashing Background
    Made a weird flashing background with javascript

--- functions.js
+++ functions.js
@@ -20,3 +20,25 @@ function getTime() {
 }
 
 //setInterval(getTime, 1000);
+
+function background() {
+    if (i>=0.9) {
+        direction = false;
+    }
+
+    if (i<=0.7) {
+        direction = true;
+    }
+
+    if (direction==false) {
+        i-=0.0005;
+    } else {
+        i+=0.0005;
+    }
+
+
+b.style.background = "rgba(9,155,102," + i + ")";
+
+//b.innerHTML = i;
+// rgb(9,155,102)
+}
