3d1f94468661cabfc69709cb142113749cb10f0d
Sean Lane <seantlane1999@gmail.com>
2017-03-08 19:26:44 +0000

    Page/modal swapping functionality and attempts at interval control

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-function getTime() {
+function printTime() {
     var time = new Date();
     var hours = time.getHours();
     var minutes = time.getMinutes();
@@ -21,16 +21,10 @@ function getTime() {
 
     var timer = document.getElementById("timer");
     timer.innerHTML = output;
-
-
-    if (minutes==39&&seconds==00) {
-        alert("yo nigga pack up");
-    }
 }
 
-//setInterval(getTime, 1000);
-
 function changeBG() {
+    
     if (i>9) {
         direction = false;
     }
@@ -54,24 +48,37 @@ function fadeIn() {
   document.getElementById("center-container").style.opacity = "1";
 }
 
-function screenSwap() {
+function screenSwap() { //function called on "Go!" button and "X" button
   var frontContent = document.getElementById("front-content"); //creates an easy-to-write variable for the front content
-  var timerContent = document.getElementById("timer-content"); //same thing for timer content
-
-  frontContent.style.opacity = "0"; //causes the front content to fade out, but remain on the page
-  setTimeout(function(){frontContent.style.display="none"; timerContent.style.display="block"; timerContent.style.opacity="1";}, 200); //after fadeout, sets front display to none and timer display to block
-  //timerContent.style.opacity = "1"; //fades in the timer
-
-  /*if (timerContent.style.display=="none") {
-    frontContent.style.opacity = "0"; //causes the front content to fade out, but remain on the page
-    setTimeout(function(){frontContent.style.display="none";}, 200); //sets display to none after fadeout
-    timerContent.style.display= "block";
-  } else {
-    timerContent.style.opacity = "0";
-    setTimeout(function(){timerContent.style.display="none";}, 200);
-    setTimeout(function(){frontContent.style.display="block";}, 200);
-    frontContent.style.opacity = "1";
-  }*/
-
-
+  var timerContent = document.getElementById("timer-content"); //creates an easy-to-write variable for the timer content
+  var frontDisplay = frontContent.style.display;
+  var timerDisplay = timerContent.style.display;
+  
+  function toTimer() {
+      frontContent.style.display="none";
+      timerContent.style.display="block";
+      timerContent.style.opacity="1";
+  }
+  
+  function toFront() {
+      timerContent.style.display="none";
+      frontContent.style.display="block";
+      frontContent.style.opacity="1";
+  }
+  
+  if (timerDisplay=="none") {
+     frontContent.style.opacity = "0"; //causes the front content to fade out, but remain on the page
+     setTimeout(toTimer, 200); //after fadeout, sets front display to none and timer display to block
+     var getTimeInterval = setInterval(printTime, 1000); //prints the time
+  //setTimeout(function(){document.getElementById("timer").innerHTML = frontDisplay;}, 1200);
+  //setTimeout(function(){document.getElementById("timer").innerHTML = timerDisplay;}, 1200); //testing to see display property value (wont change to block???!!)
+  //timerContent.style.opacity = "1"; //fades in the timer. not working for some reason :(
+  }
+  
+  if (frontDisplay=="none") {
+     timerContent.style.opacity = "0";
+     setTimeout(toFront, 200);
+     clearInterval(getTimeInterval); //stops the printing of the time but isnt stopping it :%
+  }
+    
 }
